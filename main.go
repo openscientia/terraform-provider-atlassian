94ed3c1735b2e6188f8c26122d45bab7140f56fa
iacb <iacabezasbaculima@outlook.com>
2022-07-16 00:56:42 +0100

    feat: add provider package and tf registry manifest.

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	atlassian "github.com/openscientia/terraform-provider-atlassian/internal/provider"
+)
+
+var (
+	// these will be set by the goreleaser configuration
+	// to appropriate values for the compiled binary
+	version string = "dev"
+)
+
+func main() {
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := providerserver.ServeOpts{
+		// TODO: Update this string with the published name of your provider.
+		Address: "registry.terraform.io/openscientia/atlassian",
+		Debug:   debug,
+	}
+
+	err := providerserver.Serve(context.Background(), atlassian.New(version), opts)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
