77704979324a173d19e6cbed74d1bbd93c3dadc0
iacb <35937295+iacabezasbaculima@users.noreply.github.com>
2022-08-06 19:04:07 +0100

    feat(new-data-source): `atlassian_jira_issue_field_configuration` (#71)
    * feat(new-data-source): add new data-source.
    
    * test(new-data-source): add acceptance tests.
    
    * docs(examples): add example configuration.
    
    * docs(templates): add markdown template.
    
    * docs(registry): add new data-source documentation.
    
    * chore(changelog): add changelog entry.

---
+++ examples/data-sources/atlassian_jira_issue_field_configuration/basic.tf
@@ -0,0 +1,3 @@
+data "atlassian_jira_issue_field_configuration" "example" {
+  id = "10000" # id of default issue field configuration
+}
---
+++ internal/provider/data_source_jira_issue_field_configuration_test.go
@@ -0,0 +1,41 @@
+package atlassian
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccJiraIssueFieldConfigurationDataSource_Basic(t *testing.T) {
+	resourceName := acctest.RandomWithPrefix("tf-test-issue-field-configuration")
+	dataSourceName := "data.atlassian_jira_issue_field_configuration.test"
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIssueFieldConfigurationDataSourceConfig_basic(dataSourceName, resourceName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dataSourceName, "name", resourceName),
+					resource.TestCheckResourceAttr(dataSourceName, "description", ""),
+				),
+			},
+		},
+	})
+}
+
+func testAccIssueFieldConfigurationDataSourceConfig_basic(dataSourceName, name string) string {
+	splits := strings.Split(dataSourceName, ".")
+	return fmt.Sprintf(`
+	resource %[1]q %[2]q {
+		name = %[3]q
+	  }
+	  
+	  data %[1]q %[2]q {
+		id = %[1]s.%[2]s.id
+	  }
+	`, splits[1], splits[2], name)
+}
